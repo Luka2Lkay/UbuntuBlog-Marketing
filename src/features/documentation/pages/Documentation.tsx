ea9907c5a185ca3222de367fdd0b5816dcfd3560
Luka <luka.matshebelele@gmail.com>
2026-08-26 17:34:01 +0200

    documentatin cta

--- src/features/documentation/pages/Documentation.tsx
+++ src/features/documentation/pages/Documentation.tsx
@@ -1,11 +1,13 @@
 import DocumentationHero from "@/features/documentation/components/DocumentationHero"
 import IntegrationSteps from "@/features/documentation/components/IntegrationSteps"
+import DocumentationCTA from "@/features/documentation/components/DocumentationCTA"
 
 function Documentation() {
   return (
     <>
       <DocumentationHero />
       <IntegrationSteps />
+      <DocumentationCTA />
     </>
   )
 }
